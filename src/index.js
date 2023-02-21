f918f14c3d6080fe3e96f4c4f062770a7e3293bc
ankitsingh711 <developerankit2127@gmail.com>
2023-02-21 12:41:15 +0530

    done chnages to the portfolio

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const resume_btn = document.querySelector("#resume-button-2");
+
+// resume_btn.setAttribute("target", "_blank");
+resume_btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.href = "https://drive.google.com/u/0/uc?id=1O02_J76ogTkuPfvMFxv9Z4omK3mtfGWW&export=download";
+})
