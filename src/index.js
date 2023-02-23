0ea620a062c3b536e12b517bccd5b05eacc08887
ankit711 <itsankitsingh711@edureka.co>
2023-02-23 14:46:20 +0530

    maintained changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const resume_btn = document.querySelector("#resume-button-2");
 
-// resume_btn.setAttribute("target", "_blank");
+
 resume_btn.addEventListener("click", (e) => {
     e.preventDefault();
+    window.open("https://drive.google.com/file/d/1O02_J76ogTkuPfvMFxv9Z4omK3mtfGWW/view?usp=sharing");
     window.location.href = "https://drive.google.com/u/0/uc?id=1O02_J76ogTkuPfvMFxv9Z4omK3mtfGWW&export=download";
 })
