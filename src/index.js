6508aeee39d45adaae96a4a9088f61c39ed671d7
ankitsingh711 <developerankit2127@gmail.com>
2023-06-04 22:06:19 +0530

    done all functionalities

--- src/index.js
+++ src/index.js
@@ -18,3 +18,31 @@ resume_btn2.addEventListener("click", (e) => {
   window.location.href =
     "https://drive.google.com/u/0/uc?id=1O02_J76ogTkuPfvMFxv9Z4omK3mtfGWW&export=download";
 });
+
+const design = document.querySelector("#designation");
+
+const skills = ["Full Stack Web Developer", "YouTuber"];
+let j = 0;
+function changeTxt() {
+  design.innerHTML = null;
+  var txt = skills[j];
+  if (j == skills.length - 1) {
+    j = 0;
+  } else {
+    j++;
+  }
+
+  var i = 0;
+  var speed = 100;
+
+  function typeWriter() {
+    if (i < txt.length) {
+      design.innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    }
+  }
+  typeWriter();
+}
+
+setInterval(changeTxt, 3000);
